8c86a5a5d284df24991aece8444a0b4b53ce150f
Pras <prasetyaarfanto@gmail.com>
2023-02-09 09:56:07 +0700

    Add function to communicate with open ai

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module vita-message-service
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/PullRequestInc/go-gpt3 v1.1.11
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"github.com/PullRequestInc/go-gpt3"
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func init() {
@@ -14,5 +18,19 @@ func init() {
 }
 
 func main() {
+	openAiKey := os.Getenv("OPENAIKEY")
+	if openAiKey == "" {
+		log.Fatalln("Missing OPEN AI API KEY")
+	}
 
+	ctx := context.Background()
+	openAiClient := gpt3.NewClient(openAiKey, gpt3.WithDefaultEngine(gpt3.TextDavinci003Engine))
+
+	resp, err := openAiClient.Completion(ctx, gpt3.CompletionRequest{
+		Prompt: []string{"How are you today ?"},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(resp.Choices[0].Text)
 }
