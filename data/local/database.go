726e133d2c8da708067a39338e384a4a2a0e2632
Pras <prasetyaarfanto@gmail.com>
2023-02-09 10:56:16 +0700

    Add database.go

---
+++ data/local/database.go
@@ -0,0 +1,35 @@
+package local
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"log"
+	"os"
+)
+
+func GetDB() *sql.DB {
+	var db *sql.DB
+
+	cfg := mysql.Config{
+		User:   os.Getenv("DBUSER"),
+		Passwd: os.Getenv("DBPASS"),
+		Net:    "tcp",
+		Addr:   "127.0.0.1:3306",
+		DBName: "message",
+	}
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected !!")
+
+	return db
+}
