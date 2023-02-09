c288f4470f3183cbfe9a35f614457b18840e977a
Pras <prasetyaarfanto@gmail.com>
2023-02-09 12:02:54 +0700

    Add openai.go

---
+++ data/network/openai.go
@@ -0,0 +1,17 @@
+package network
+
+import (
+	"github.com/PullRequestInc/go-gpt3"
+	"log"
+	"os"
+)
+
+func GetOpenAi() *gpt3.Client {
+	openAiKey := os.Getenv("OPENAIKEY")
+	if openAiKey == "" {
+		log.Fatalln("Missing OPEN AI API KEY")
+	}
+
+	openAiClient := gpt3.NewClient(openAiKey, gpt3.WithDefaultEngine(gpt3.TextDavinci003Engine))
+	return &openAiClient
+}
