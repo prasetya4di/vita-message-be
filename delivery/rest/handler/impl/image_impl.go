1d4af468f368306fa1f5bc39ad8108da5f80e218
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-02-15 17:47:17 +0700

    Add upload image handler

---
+++ delivery/rest/handler/impl/image_impl.go
@@ -0,0 +1,45 @@
+package impl
+
+import (
+	"github.com/gin-gonic/gin"
+	"mime/multipart"
+	"net/http"
+	"vita-message-service/delivery/rest/handler"
+	"vita-message-service/usecase"
+)
+
+type imageHandler struct {
+	uploadImage usecase.UploadImage
+}
+
+func NewImageHandler(uploadImage usecase.UploadImage) handler.ImageHandler {
+	return &imageHandler{
+		uploadImage,
+	}
+}
+
+func (ih *imageHandler) UploadImage(c *gin.Context) {
+	file, header, err := c.Request.FormFile("image")
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": err})
+		return
+	}
+	if !isValidImage(file) {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid image format"})
+		return
+	}
+	email := c.Param("email")
+	possibilities, err := ih.uploadImage.Invoke(email, file, header)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, possibilities)
+}
+
+func isValidImage(file multipart.File) bool {
+	buff := make([]byte, 512)
+	file.Read(buff)
+	contentType := http.DetectContentType(buff)
+	return contentType == "image/png" || contentType == "image/jpg" || contentType == "image/gif" || contentType == "image/webp" || contentType == "image/jpeg" || contentType == "image/bmp"
+}
