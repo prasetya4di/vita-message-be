a8082803fb99cfdf805a5174e028a6f3688818b9
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-02-15 17:48:09 +0700

    Add route for image

--- delivery/rest/router.go
+++ delivery/rest/router.go
@@ -7,11 +7,14 @@ import (
 	"vita-message-service/delivery/rest/handler"
 )
 
-func LoadRoutes(mh handler.MessageHandler) {
+func LoadRoutes(mh handler.MessageHandler, ih handler.ImageHandler) {
 	router := gin.Default()
 	router.POST("/message", mh.SendMessage)
 	router.POST("/message/reply", mh.ReplyMessage)
 	router.GET("/message/:email", mh.GetMessage)
+
+	router.POST("/image/:email", ih.UploadImage)
+	router.Static("/image", "upload/images")
 	err := router.Run(os.Getenv("BASEURL") + ":" + os.Getenv("PORT"))
 	if err != nil {
 		log.Fatal(err)
