6df5fe635e7e5ad24bb691003bf523c78897ab89
Pras <prasetyaarfanto@gmail.com>
2023-02-20 23:51:34 +0700

    Add user repository

---
+++ repository/impl/user_impl.go
@@ -0,0 +1,27 @@
+package impl
+
+import (
+	"vita-message-service/data/entity"
+	"vita-message-service/data/local"
+	"vita-message-service/repository"
+)
+
+type userRepository struct {
+	userDao local.UserDao
+}
+
+func NewUserRepository(dao local.UserDao) repository.UserRepository {
+	return &userRepository{userDao: dao}
+}
+
+func (ur *userRepository) Login(email string, password string) (entity.User, error) {
+	return ur.userDao.Login(email, password)
+}
+
+func (ur *userRepository) Register(user entity.User) (entity.User, error) {
+	return ur.userDao.Register(user)
+}
+
+func (ur *userRepository) Get(email string) entity.User {
+	return ur.userDao.Get(email)
+}
---
+++ repository/user.go
@@ -0,0 +1,9 @@
+package repository
+
+import "vita-message-service/data/entity"
+
+type UserRepository interface {
+	Login(email string, password string) (entity.User, error)
+	Register(user entity.User) (entity.User, error)
+	Get(email string) entity.User
+}
