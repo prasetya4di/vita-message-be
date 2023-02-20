da023847bc243147508e7491522de1802318fb33
Pras <prasetyaarfanto@gmail.com>
2023-02-20 23:40:04 +0700

    Ignore password from json

--- data/entity/user.go
+++ data/entity/user.go
@@ -3,10 +3,10 @@ package entity
 import "time"
 
 type User struct {
-	Email     string    `json:"email"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Nickname  string    `json:"nickname"`
-	Password  string    `json:"password"`
+	Email     string `json:"email"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Nickname  string `json:"nickname"`
+	Password  string
 	BirthDate time.Time `json:"birth_date"`
 }
