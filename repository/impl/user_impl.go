1538655c64a05c54d7baeae825660924dac2d57a
Pras <prasetyaarfanto@gmail.com>
2023-02-21 11:03:23 +0700

    Update user repository

--- repository/impl/user_impl.go
+++ repository/impl/user_impl.go
@@ -14,14 +14,14 @@ func NewUserRepository(dao local.UserDao) repository.UserRepository {
 	return &userRepository{userDao: dao}
 }
 
-func (ur *userRepository) Login(email string, password string) (entity.User, error) {
+func (ur *userRepository) Login(email string, password string) (*entity.User, error) {
 	return ur.userDao.Login(email, password)
 }
 
-func (ur *userRepository) Register(user entity.User) (entity.User, error) {
+func (ur *userRepository) Register(user entity.User) (*entity.User, error) {
 	return ur.userDao.Register(user)
 }
 
-func (ur *userRepository) Get(email string) entity.User {
+func (ur *userRepository) Get(email string) *entity.User {
 	return ur.userDao.Get(email)
 }
