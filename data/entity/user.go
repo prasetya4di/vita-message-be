2eee98301c8ee17708e71e9ca3058c7ad53be563
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-02-22 16:36:44 +0700

    Remove unique

--- data/entity/user.go
+++ data/entity/user.go
@@ -8,9 +8,9 @@ import (
 type User struct {
 	gorm.Model
 	Email     string    `json:"email" gorm:"size:128; not null; unique"`
-	FirstName string    `json:"first_name" gorm:"size:50; not null; unique"`
-	LastName  string    `json:"last_name" gorm:"size:50; not null; unique"`
-	Nickname  string    `json:"nickname" gorm:"size:50; not null; unique"`
-	Password  string    `json:"password" gorm:"size:128; not null; unique"`
+	FirstName string    `json:"first_name" gorm:"size:50; not null"`
+	LastName  string    `json:"last_name" gorm:"size:50; not null"`
+	Nickname  string    `json:"nickname" gorm:"size:50; not null"`
+	Password  string    `json:"password" gorm:"size:128; not null"`
 	BirthDate time.Time `json:"birth_date" gorm:"not null"`
 }
