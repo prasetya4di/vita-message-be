b004778a73e115df1800a685dbcb6ff32b0f18e4
Prasetya Adi Arfanto <84487845+prasetya4di@users.noreply.github.com>
2023-03-22 20:43:34 +0700

    Add energy struct

---
+++ data/entity/energy.go
@@ -0,0 +1,13 @@
+package entity
+
+import (
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+type Energy struct {
+	gorm.Model
+	Email       string    `json:"email" gorm:"size:128; not null"`
+	Energy      uint      `json:"energy" gorm:"type:uint; not null"`
+	ExpiredDate time.Time `json:"expired_date" gorm:"not null"`
+}
