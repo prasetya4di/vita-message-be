68ee87623294aa4aae9246fa8097e90843e23299
Prasetya <prasetyaarfanto@gmail.com>
2024-05-19 20:58:22 +0700

    Remove unused entity

--- data/entity/user.go
+++ data/entity/user.go
@@ -12,7 +12,6 @@ type User struct {
 	Password  string    `json:"password" gorm:"size:128; not null"`
 	BirthDate time.Time `json:"birth_date" gorm:"not null"`
 	Message   []Message `json:"messages" gorm:"foreignKey:email; references:email"`
-	Energy    Energy    `json:"energy" gorm:"foreignKey:email; references:email"`
 }
 
 func (User) TableName() string {
